1ddfb2127402d9f47b546c6de5bc18dd32529625
FruitsL <sa4560@naver.com>
2025-10-30 17:02:09 +0900

    Update README.md to enhance project overview and functionality details; modify startup.sh for improved package installation

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+apt-get update
+apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 ffmpeg
+pip install --no-cache-dir -r requirements.txt
+pip install deepface --no-deps
 streamlit run main.py --server.port=8000 --server.address=0.0.0.0 --server.enableCORS=false
